508152ab9a85d8cfd17f206067fd478c7588f75a
Stefan Moravcik <stefan@virsas.com>
2022-04-25 19:36:47 +0200

    added region

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.region
+}
+
 resource "aws_key_pair" "key" {
   key_name   = var.key.name
   public_key = var.key.key
--- variables.tf
+++ variables.tf
@@ -1,1 +1,2 @@
+variable "region" { default = "eu-west-1" }
 variable "key" {default = { name = "example", key = "ssh-ed25519 AAAA1234567890 user@example.org"}}
